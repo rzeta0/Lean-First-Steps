517829ad9eff85af094f775b8af80291ff978614
Tariq Rashid <tariq.rashid50@gmail.com>
2024-06-15 13:26:43 +0100

    cleanup

--- 02_second.lean
+++ 02_second.lean
@@ -5,4 +5,4 @@ import Mathlib.Tactic
 example {x y z c: ℝ} (h1 : z = y^2) (h2: y = x + c) : z = (x + c)^2 :=
   calc
     z = y^2 := by rw [h1]
-    _ = (x+c)^2 := by rw [h2]
+    _ = (x + c)^2 := by rw [h2]
