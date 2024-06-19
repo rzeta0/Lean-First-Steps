ebcfa298cab322a5fde91619cefeb945eca59c06
Tariq Rashid <tariq.rashid50@gmail.com>
2024-06-19 23:59:48 +0100

    update

--- 04_algebra.lean
+++ 04_algebra.lean
@@ -2,14 +2,6 @@
 
 import Mathlib.Tactic
 
-
-example {x y z c: ℝ} (h1 : z = y^2) (h2: y = x + c) : z = x^2 + c^2 + 2*x*c :=
-  calc
-    z = y^2 := by rw [h1]
-    _ = (x+c)^2 := by rw [h2]
-    _ = x^2 + c^2 + 2*x*c := by ring
-
-
 example {y a b : ℝ} (h1 : y = (a - b) * (a + b)) : y = a^2 - b^2 :=
   calc
     y = (a - b) * (a + b) := by rw [h1]
