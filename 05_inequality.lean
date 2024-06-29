2932a93191da7894a1caccf91c1f51ab684d781b
Tariq Rashid <tariq.rashid50@gmail.com>
2024-06-29 23:48:59 +0100

    inequality in the hypotheses

--- 05_inequality.lean
+++ 05_inequality.lean
@@ -2,13 +2,6 @@
 
 import Mathlib.Tactic
 
-example {a : ℕ} (h1 : a = 2) : a^2 ≥ 4 :=
-  calc
-    a^2 = 2^2 := by rw [h1]
-    _ = 4 := by ring
-    _ ≥ 4 := by norm_num
-
-
 example {a b : ℕ} (h1 : b = a^2) (h2: a ≥ 2) : b ≥ 4 :=
   calc
     b = a^2 := by rw [h1]
