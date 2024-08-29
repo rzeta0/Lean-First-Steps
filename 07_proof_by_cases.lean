da3a6621c9afe70d6f8e8f66dcddc79331be3112
Tariq Rashid <tariq.rashid50@gmail.com>
2024-08-29 21:55:46 +0100

    use dots

--- 07_proof_by_cases.lean
+++ 07_proof_by_cases.lean
@@ -4,9 +4,9 @@ import Mathlib.Tactic
 
 example {x : ℤ} (h : x = 3 ∨ x = -3) : x^2 = 9 := by
   obtain ha | hb := h
-  calc
+  · calc
     x^2 = (3)^2 := by rw [ha]
     _ = 9 := by norm_num
-  calc
+  · calc
     x^2 = (-3)^2 := by rw [hb]
     _ = 9 := by norm_num
