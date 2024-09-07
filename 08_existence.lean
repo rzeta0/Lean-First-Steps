fc3bdd1ff32defd5a8252cceb7935c60b09137ee
Tariq Rashid <tariq.rashid50@gmail.com>
2024-09-07 16:25:38 +0100

    space2

--- 08_existence.lean
+++ 08_existence.lean
@@ -5,7 +5,7 @@ import Mathlib.Tactic
 example : ∃ n: ℕ , n^2 + 1 = 10 := by
   use 3
   calc
-    3^2 + 1  = 10 := by norm_num
+    3^2 + 1 = 10 := by norm_num
 
 -- more concise
 example : ∃ n: ℕ , n^2 + 1 = 10 := by
