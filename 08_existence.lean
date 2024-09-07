60056666209741ffb59191fab6823e91c295b3c4
Tariq Rashid <tariq.rashid50@gmail.com>
2024-09-07 16:24:42 +0100

    x to n

--- 08_existence.lean
+++ 08_existence.lean
@@ -2,12 +2,12 @@
 
 import Mathlib.Tactic
 
-example : ∃ x: ℕ , x^2 + 1 = 10 := by
+example : ∃ n: ℕ , x^2 + 1 = 10 := by
   use 3
   calc
     3^2+ 1  = 10 := by norm_num
 
 -- more concise
-example : ∃ x: ℕ , x^2 + 1 = 10 := by
+example : ∃ n: ℕ , x^2 + 1 = 10 := by
   use 3
   norm_num
