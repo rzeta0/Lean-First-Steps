af38361cf8bbdbda485eaa62bcb11ba00425b5d3
Tariq Rashid <tariq.rashid50@gmail.com>
2024-09-23 12:34:17 +0100

    new ex 10

--- exercise_solutions.lean
+++ exercise_solutions.lean
@@ -94,6 +94,22 @@ example {x : ℤ} (h : x = -1) : x = 1 ∨ x^2 = 1 ∨ x^3 = 1 := by
     _ = 1 := by norm_num
 
 
+-- 10 - Conjunctive "and" Goal
+-- Write a Lean program to show that (x^3 = -1) ∧ (x^4 = 1) ∧ (x^5 = -1) given integer x = -1.
+
+example {x : ℤ} (h : x = -1) : x^3 = -1 ∧ x^4 = 1 ∧ x^5 = -1:= by
+  constructor
+  · calc
+      x^3 = (-1)^3 := by rw [h]
+      _ = -1 := by norm_num
+  · constructor
+    · calc
+        x^4 = (-1)^4 := by rw [h]
+        _ = 1 := by norm_num
+    · calc
+        x^5 = (-1)^5 := by rw [h]
+        _ = -1 := by norm_num
+
 
 -- 11 - Existence
 -- Write a Lean program to prove there exists a natural number larger than 5.
