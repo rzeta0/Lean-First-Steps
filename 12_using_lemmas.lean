bb0bf951fc38235a5ed8b84ef5553031219501c9
Tariq Rashid <tariq.rashid50@gmail.com>
2024-09-28 20:12:45 +0100

    3 to 5

--- 12_using_lemmas.lean
+++ 12_using_lemmas.lean
@@ -2,8 +2,8 @@
 
 import Mathlib.Tactic
 
-example {n : ℕ} (h: n = 1): n ≠ 3 := by
+example {n : ℕ} (h: n = 1): n ≠ 5 := by
   apply ne_of_lt
   calc
     n = 1 := by rw [h]
-    _ < 3 := by norm_num
+    _ < 5 := by norm_num
