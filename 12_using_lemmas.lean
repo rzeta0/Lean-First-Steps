623f7f8888f233ec21262341c7f44b90cd2ff7bb
Tariq Rashid <tariq.rashid50@gmail.com>
2024-09-30 09:46:44 +0100

    more ideas for apply

--- 12_using_lemmas.lean
+++ 12_using_lemmas.lean
@@ -7,18 +7,3 @@ example {n : ℕ} (h: n = 1): n ≠ 5 := by
   calc
     n = 1 := by rw [h]
     _ < 5 := by norm_num
-
----
-
-example {n : ℕ} (h: n = 1): n ≠ 5 := by
-  calc
-    n = 1 := by rw [h]
-    _ ≠ 5 := by norm_num
-
----
-
-example {n : ℕ} (h: n < 5): n ≠ 5 := by
-  apply ne_of_lt
-  apply h
-
---
