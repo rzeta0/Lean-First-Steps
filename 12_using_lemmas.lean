449b382aadde2615e7bf472cd3749ec04f51e3e9
Tariq Rashid <tariq.rashid50@gmail.com>
2024-09-30 22:45:23 +0100

    longer calc section

--- 12_using_lemmas.lean
+++ 12_using_lemmas.lean
@@ -5,3 +5,11 @@ import Mathlib.Tactic
 example {n : ℕ} (h: n < 5): n ≠ 5 := by
   apply ne_of_lt
   apply h
+
+
+-- longer version with a calc section
+
+example {n : ℕ} (h: n < 5): n ≠ 5 := by
+  apply ne_of_lt
+  calc
+    n < 5 := by rel [h]
