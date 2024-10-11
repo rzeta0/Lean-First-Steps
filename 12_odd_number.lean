ddeaef737a09a441cb8802faa9efe5d8b7291047
Tariq Rashid <tariq.rashid50@gmail.com>
2024-10-11 15:39:15 +0100

    Odd

--- 12_odd_number.lean
+++ 12_odd_number.lean
@@ -2,15 +2,7 @@
 
 import Mathlib.Tactic
 
-example  : Odd 13 := by
+example : Odd (13: ℤ)  := by
   dsimp [Odd]
   use 6
-  calc
-    13 = 2*6 + 1 := by ring
-
--- shorter version
-
-example  : Odd 13 := by
-  dsimp [Odd]
-  use 6
-  ring
+  norm_num
