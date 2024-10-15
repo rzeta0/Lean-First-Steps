eedd303e64e59a485d676f597afe82ac2acc314b
Tariq Rashid <tariq.rashid50@gmail.com>
2024-10-15 22:01:51 +0100

    simplify 13

--- 13_lemma_ne_of_lt.lean
+++ 13_lemma_ne_of_lt.lean
@@ -5,11 +5,3 @@ import Mathlib.Tactic
 example {n : ℕ} (h: n < 5): n ≠ 5 := by
   apply ne_of_lt
   exact h
-
-
--- longer version with a calc section
-
-example {n : ℕ} (h: n < 5): n ≠ 5 := by
-  apply ne_of_lt
-  calc
-    n < 5 := by rel [h]
