0e95cad86788e4620143d55c1e60d58cfd2c4aee
Tariq Rashid <tariq.rashid50@gmail.com>
2024-10-17 01:43:26 +0100

    odd even lemmas

--- 00_scratch.lean
+++ 00_scratch.lean
@@ -11,15 +11,49 @@ example {n : ℕ} : ∃ k: ℕ, n^2 + n = 2*k := by
   · use 2*m^2 + m
     calc
       n^2 + n = (2*m)^2 + (2*m) := by rw [ha]
-      _ = 4*m^2 + 2*m := by ring
       _ = 2*(2*m^2 + m) := by ring
   · use 2*m^2 + 3*m + 1
     calc
       n^2 + n = (2*m + 1)^2 + (2*m + 1) := by rw [hb]
-      _ = 4*m^2 + 4*m + 1 + 2*m + 1 := by ring
       _ = 2*(2*m^2 + 3*m + 1) := by ring
 
 --
+
+-- alternative using Odd and Even
+-- remember to do an Int version from Nat
+
+example {n : ℤ} : Even (n^2 + n) := by
+  have h: Even n ∨ Odd n := Int.even_or_odd n
+  obtain ha | hb := h
+  · dsimp [Odd, Even] at *
+    obtain ⟨x, hx⟩ := ha
+    use 2*x^2 + x
+    calc
+      n^2 + n = (x + x)^2 + (x + x) := by rw [hx]
+      _ = (2*x^2 + x) + (2*x^2 + x) := by ring
+  · dsimp [Odd, Even] at *
+    obtain ⟨y, hy⟩ := hb
+    use 2*y^2 + 3*y + 1
+    calc
+      n^2 + n = (2*y + 1)^2 + (2*y + 1) := by rw [hy]
+      _ = (2*y^2 + 3*y + 1) + (2*y^2 + 3*y + 1) := by ring
+
+-----
+
+-- from leanprover zulip
+
+example {n : ℤ} : Even (n^2 + n) := by
+  have  g: n^2 = n * n := by ring
+  rw [g]
+  obtain ha | hb := Int.even_or_odd n
+  · apply Even.add
+    · apply Even.mul_left ha
+    · apply ha
+  · apply Odd.add_odd
+    · apply Odd.mul hb hb
+    · apply hb
+
+-----
 
 
 example {n : ℕ} : n^2 ≠ 2 := by
@@ -50,25 +84,6 @@ example {n : ℕ} : n^2 ≠ 2 := by
     calc
       n^2 ≥ (2)^2 := by rel [hb]
       _ > 2 := by norm_num
-
---
-
--- definition
-
-example {a : ℕ} (h: a = 13) : Odd a := by
-  dsimp [Odd]
-  use 6
-  calc
-    a = 13 :=  by rw [h]
-    _ = 2*6 + 1 := by ring
-
-
-example  : Odd 13 := by
-  dsimp [Odd]
-  use 6
-  calc
-    13 = 2*6 + 1 := by ring
-
 ---
 
 -- not_prime_pow
@@ -109,3 +124,12 @@ example {a : ℕ} (h: a = 9) : ¬ Prime (a) := by
   apply IsSquare.not_prime h1
 
 ---
+
+example : ¬ Nat.Prime (12) := by
+  have g: 12 = 3*4 := by norm_num
+  rw [g]
+  have h1: 3 ≠ 1 := by norm_num
+  have h2: 4 ≠ 1 := by norm_num
+  apply Nat.not_prime_mul h1 h2
+
+--
