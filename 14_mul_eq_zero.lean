bdb4ce8288ca46f89bd1c969cb498e3c073c2ba6
Tariq Rashid <tariq.rashid50@gmail.com>
2024-10-23 00:35:47 +0100

    mul_eq_zero

--- 14_mul_eq_zero.lean
+++ 14_mul_eq_zero.lean
@@ -2,25 +2,9 @@
 
 import Mathlib.Tactic
 
-example {a b : ℚ} (h: (a - 1) * (b - 2) = 0): a = 1 ∨ b = 2 := by
+example {p q : ℚ} (h: (p - 1) * (q - 2) = 0): p = 1 ∨ q = 2 := by
   rw [mul_eq_zero] at h
-  obtain ha | hb := h
-  · left
-    calc
-      a = a - 1 + 1 := by ring
-      _ = 0 + 1 := by rw [ha]
-      _ = 1 := by norm_num
-  · right
-    calc
-      b = b - 2 + 2 := by ring
-      _ = 0 + 2 := by rw [hb]
-      _ = 2 := by norm_num
-
---
-
-example {a b : ℚ} (h: (a - 1) * (b - 2) = 0): a = 1 ∨ b = 2 := by
-  rw [mul_eq_zero] at h
-  obtain ha | hb := h
+  obtain hp | hq := h
   · left
     linarith
   · right
