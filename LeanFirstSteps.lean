6b3d269a03f8c2e1cda748cc2f94e0e35d1baed4
rzeta0 <tariqrashid50@gmail.com>
2024-11-16 14:21:03 +0000

    updated lemma

--- LeanFirstSteps.lean
+++ LeanFirstSteps.lean
@@ -3,11 +3,11 @@ import Mathlib.Tactic
 ---
 
 lemma Nat.le_or_succ_le (a b: ℕ): a ≤ b ∨ b + 1 ≤ a := by
-  rw [Nat.succ_le]
+  rw [Nat.add_one_le_iff]
   exact le_or_lt a b
 
-example {a : ℕ} :  a ≤ 2 ∨ 3 ≤ a  := by
-  apply Nat.le_or_succ_le a 2
+example {c : ℕ} :  c ≤ 2 ∨ 3 ≤ c  := by
+  exact Nat.le_or_succ_le c 2
 
 ---
 
