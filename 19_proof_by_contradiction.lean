9b8ca5c5704da7929d417cf1c12ab4fa47946a02
rzeta0 <tariqrashid50@gmail.com>
2024-12-13 16:16:13 +0000

    tidy up

--- 19_proof_by_contradiction.lean
+++ 19_proof_by_contradiction.lean
@@ -5,6 +5,5 @@ import Mathlib.Tactic
 example {a b : ℕ} (h1: a = 5 → b = 6) (h2: b = 7) : ¬ a = 5 := by
   intro g
   apply h1 at g
-  have g1 : b ≠ 7 := by linarith
-  rw [ne_eq] at g1
+  have g1 : ¬ b = 7 := by linarith
   contradiction
