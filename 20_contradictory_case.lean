4eda52f44299ed3eb5639b7bce4c5f95d4f7d0f8
rzeta0 <tariqrashid50@gmail.com>
2024-12-16 17:51:01 +0000

    contradiction with cases

--- 20_contradictory_case.lean
+++ 20_contradictory_case.lean
@@ -8,3 +8,16 @@ example {x : ℝ} (h1 : (x + 3) * (x - 3) = 0) (h2 : ¬ x = 3) : x = - 3 := by
   · linarith
   · have gc: x = 3 := by linarith
     contradiction
+
+
+example {x : ℝ} (h1 : (x + 3) * (x - 3) = 0) (h2 : x > 0) : x = 3 := by
+  rw [mul_eq_zero] at h1
+  obtain ga | gb := h1
+  · have gx := by
+      calc
+        x = -3 := by linarith
+        _ < 0 := by norm_num
+    have gx2: ¬ x > 0 := by linarith [gx]
+    contradiction
+  · linarith
+
