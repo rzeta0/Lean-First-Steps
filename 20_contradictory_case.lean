d03af69e90c6f7faa3893db8805065d555f08c32
rzeta0 <tariqrashid50@gmail.com>
2024-12-18 17:18:09 +0000

    clean up

--- 20_contradictory_case.lean
+++ 20_contradictory_case.lean
@@ -2,22 +2,8 @@
 
 import Mathlib.Tactic
 
-example {x : ℝ} (h1 : (x + 3) * (x - 3) = 0) (h2 : ¬ x = 3) : x = - 3 := by
-  rw [mul_eq_zero] at h1
-  obtain ga | gb := h1
-  · linarith
-  · have gc: x = 3 := by linarith
-    contradiction
-
-
-example {x : ℝ} (h1 : (x + 3) * (x - 3) = 0) (h2 : x > 0) : x = 3 := by
-  rw [mul_eq_zero] at h1
-  obtain ga | gb := h1
-  · have gx := by
-      calc
-        x = -3 := by linarith
-        _ < 0 := by norm_num
-    have gx2: ¬ x > 0 := by linarith [gx]
-    contradiction
-  · linarith
-
+example (P : Prop) : ¬ (¬ P) → P := by
+  intro g
+  by_cases h1: P
+  · exact h1
+  · contradiction
