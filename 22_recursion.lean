f66d3039fcd9ae58a0b5cdc525c455cc187be5ab
rzeta0 <tariqrashid50@gmail.com>
2025-01-02 18:19:06 +0000

    1+2n to 2n + 1

--- 22_recursion.lean
+++ 22_recursion.lean
@@ -4,7 +4,7 @@ import Mathlib.Tactic
 
 def f : ℕ → ℕ
   | 0 => 0
-  | n + 1 => 1 + 2 * n + f n
+  | n + 1 => 2 * n + 1 + f n
 
 #eval f 2
 
@@ -16,6 +16,6 @@ example {n: ℕ} : f n = n^2 := by
       _ = 0^2 := by norm_num
   | succ n ih =>
     calc
-      f (n + 1) = 1 + 2 * n + f n := by rw [f]
-      _ = 1 + 2 * n + n^2 := by rw [ih]
+      f (n + 1) = 2 * n + 1 + f n := by rw [f]
+      _ = 2 * n + 1 + n^2 := by rw [ih]
       _ = (n + 1)^2 := by ring
