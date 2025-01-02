7829cd37be082b135db1e097d9df620227495cf7
rzeta0 <tariqrashid50@gmail.com>
2025-01-02 18:16:49 +0000

    fn to f

--- 22_recursion.lean
+++ 22_recursion.lean
@@ -2,20 +2,20 @@
 
 import Mathlib.Tactic
 
-def fₙ : ℕ → ℕ
+def f : ℕ → ℕ
   | 0 => 0
-  | n + 1 => 1 + 2 * n + fₙ n
+  | n + 1 => 1 + 2 * n + f n
 
-#eval fₙ 2
+#eval f 2
 
-example {n: ℕ} : fₙ n = n^2 := by
+example {n: ℕ} : f n = n^2 := by
   induction n with
   | zero =>
     calc
-      fₙ 0 = 0 := by rw [fₙ]
+      f 0 = 0 := by rw [f]
       _ = 0^2 := by norm_num
   | succ n ih =>
     calc
-      fₙ (n + 1) = 1 + 2 * n + fₙ n := by rw [fₙ]
+      f (n + 1) = 1 + 2 * n + f n := by rw [f]
       _ = 1 + 2 * n + n^2 := by rw [ih]
       _ = (n + 1)^2 := by ring
