3f3c357d7e03a7221ebe75b0d52a5da489845d98
rzeta0 <tariqrashid50@gmail.com>
2025-02-02 13:24:58 +0000

    update

--- 00_scratch.lean
+++ 00_scratch.lean
@@ -1,8 +1,3 @@
 import Mathlib.Tactic
 
-def prod_example (p : Bool × Nat) : Nat :=
-  Prod.casesOn (motive := fun _ => Nat) p (fun b n => cond b (2 * n) (2 * n + 1))
-
-#eval prod_example (true, 3)
-#eval prod_example (false, 3)
-
+#eval 2 + 4
